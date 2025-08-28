3a75a31f0dc70f015c6e933b00cdbaf21628b8d0
Luis Fernando Leyva Luna <fernando_rendel892@hotmail.com>
2025-08-28 13:31:20 -0600

    Ejercicio Fernando Leyva

--- src/WeightedUniformStrings.Core/WeightedUniformStringSolver.cs
+++ src/WeightedUniformStrings.Core/WeightedUniformStringSolver.cs
@@ -4,6 +4,27 @@ public class WeightedUniformStringSolver
 {
     public List<string> WeightedUniformStrings(string s, List<int> queries)
     {
+        List<string> conjuntoU = new List<string>();
+
+        List<string> conjuntoV = new List<string>();
+
+        int contador = 0;
+        int peso = 0;
+
+
+
+        foreach (string elemento in s.Split())
+        {
+            Console.WriteLine(elemento);
+
+            string elementoActual = elemento;
+            if (elemento == elementoActual)
+            {
+                contador++;
+            }
+
+        }
+
         return new List<string>();
     }
 }
